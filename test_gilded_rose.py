54c9b19514ccc478f809e58b62fec12ae068bc8f
unknown <chen.taoyu@northeastern.edu>
2025-02-06 00:10:59 -0800

    four failing tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,28 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_backstage_passes_to_a_TAFKAL80ETC_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(52, items[0].quality, "Backstage passes should increase 3 quality daily when less than 5 days")
+
+    def test_conjured_before_expiration(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].quality, "Conjured items should lose 2 quality daily before expiration")
+
+    def test_conjured_after_expiration(self):
+        items = [Item("Conjured Mana Cake", 0, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(2, items[0].quality, "Conjured items should lose 4 quality daily after expiration")
+
+    def test_get_all_item_names_not_implemented(self):
+        gilded_rose = GildedRose.get_all_item_names()
+        self.assertTrue(gilded_rose, list)
+
 
 if __name__ == '__main__':
     unittest.main()
