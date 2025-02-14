3b71da9df77b14b1d5804ad9926ebf7c9c1e95e7
unknown <chen.taoyu@northeastern.edu>
2025-02-13 21:59:05 -0800

    Lab 5

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,38 +12,88 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class ItemUpdater:
+    def __init__(self, item: Item):
+        self.item = item
 
+    def update(self):
+        raise NotImplementedError("Subclasses must override update() method.")
+
+
+class RegularUpdater(ItemUpdater):
+    def update(self):
+        self.decrease_quality(1)
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0:
+            self.decrease_quality(1)
+
+    def decrease_quality(self, amount):
+        self.item.quality = max(0, self.item.quality - amount)
+
+
+class AgedBrieUpdater(ItemUpdater):
+    def update(self):
+        self.increase_quality(1)
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0:
+            self.increase_quality(1)
+
+    def increase_quality(self, amount):
+        self.item.quality = min(50, self.item.quality + amount)
+
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self):
+        pass
+
+
+class BackstageUpdater(ItemUpdater):
+    def update(self):
+        if self.item.sell_in > 10:
+            self.increase_quality(1)
+        elif self.item.sell_in > 5:
+            self.increase_quality(2)
+        elif self.item.sell_in >= 0:
+            self.increase_quality(3)
+        else:
+            self.item.quality = 0
+
+        self.item.sell_in -= 1
+
+    def increase_quality(self, amount):
+        self.item.quality = min(50, self.item.quality + amount)
+
+
+class ConjuredUpdater(RegularUpdater):
+    def update(self):
+        self.decrease_quality(2)
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0:
+            self.decrease_quality(2)
+
+
+def get_updater(item: Item) -> ItemUpdater:
+    if item.name == "Aged Brie":
+        return AgedBrieUpdater(item)
+    elif item.name == "Sulfuras, Hand of Ragnaros":
+        return SulfurasUpdater(item)
+    elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+        return BackstageUpdater(item)
+    elif "Conjured" in item.name:
+        return ConjuredUpdater(item)
+    else:
+        return RegularUpdater(item)
+
+
+class GildedRose(object):
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            updater = get_updater(item)
+            updater.update()
+
+    def get_all_item_names(self):
+        return [item.name for item in self.items]
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -6,16 +6,16 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("foo", 0, 3)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(1, items[0].quality)
 
     def test_backstage_passes_to_a_TAFKAL80ETC_concert(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 45)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(52, items[0].quality, "Backstage passes should increase 3 quality daily when less than 5 days")
+        self.assertEqual(48, items[0].quality, "Backstage passes should increase 3 quality daily when less than 5 days")
 
     def test_conjured_before_expiration(self):
         items = [Item("Conjured Mana Cake", 3, 6)]
@@ -29,9 +29,16 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(2, items[0].quality, "Conjured items should lose 4 quality daily after expiration")
 
-    def test_get_all_item_names_not_implemented(self):
-        gilded_rose = GildedRose.get_all_item_names()
-        self.assertTrue(gilded_rose, list)
+    def test_get_all_item_names(self):
+        items = [
+            Item("foo", 0, 3),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 45),
+            Item("Conjured Mana Cake", 3, 6)
+        ]
+        gilded_rose = GildedRose(items)
+        names = gilded_rose.get_all_item_names()
+        expected_names = ["foo", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake"]
+        self.assertEqual(expected_names, names)
 
 
 if __name__ == '__main__':
